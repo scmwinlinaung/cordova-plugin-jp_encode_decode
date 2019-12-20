520e874d8031ada237791397ddd77fb0b5a0ce85
winlinaung <scm.winlinaung@gmail.com>
2019-12-20 16:58:37 +0630

    sixth

--- www/jp_encode_decode.js
+++ www/jp_encode_decode.js
@@ -8,11 +8,6 @@ var jp_encode_decode = function() {}; // This just makes it easier for
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
 jp_encode_decode.EncodeWithSJIS = function(Data, onSuccess, onError) {
    Data = Data;
-   Data.then(
-      data => {
-         console.log('data = ', data)
-      }
-   )
    console.log("Data = ", Data)
 
    exec(onSuccess, onError, PLUGIN_NAME, "EncodeWithSJIS", [Data]);
