4726db907c7a6f89eea8176498961e6bc3e425c8
winlinaung <scm.winlinaung@gmail.com>
2019-12-20 15:15:57 +0630

    sixth

--- www/jp_encode_decode.js
+++ www/jp_encode_decode.js
@@ -8,7 +8,7 @@ var jp_encode_decode = function() {}; // This just makes it easier for
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
 jp_encode_decode.EncodeWithSJIS = function(EncodeDecodeData, onSuccess, onError) {
    EncodeDecodeData = EncodeDecodeData || {};
-   EncodeDecodeData.text =  EncodeDecodeData.text || ""
+   EncodeDecodeData.text =  EncodeDecodeData.text || "販売名不明のインスリン製剤ブランク(空文字)"
    EncodeDecodeData.csvEncodedData = EncodeDecodeData.csvEncodedData || ""
    console.log("EncodeDecodeData = ", EncodeDecodeData)
    exec(onSuccess, onError, PLUGIN_NAME, "EncodeWithSJIS", [EncodeDecodeData.text, EncodeDecodeData.csvEncodedData]);
