54948aac26d5c6eb2b19ccaa7d8a39e55075e48c
winlinaung <scm.winlinaung@gmail.com>
2019-12-20 14:40:18 +0630

    fourth

--- www/jp_encode_decode.js
+++ www/jp_encode_decode.js
@@ -6,7 +6,12 @@ var jp_encode_decode = function() {}; // This just makes it easier for
  // us to export all of the functions at once.
 // All of your plugin functions go below this. 
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-jp_encode_decode.EncodeWithSJIS = function(onSuccess, onError) {
-   exec(onSuccess, onError, PLUGIN_NAME, "EncodeWithSJIS", []);
+jp_encode_decode.EncodeWithSJIS = function(EncodeDecodeData, onSuccess, onError) {
+   EncodeDecodeData = EncodeDecodeData || {};
+   EncodeDecodeData.text =  EncodeDecodeData.text
+   EncodeDecodeData.csvEncodedData = EncodeDecodeData.csvEncodedData
+   exec(onSuccess, onError, PLUGIN_NAME, "EncodeWithSJIS", [EncodeDecodeData.text, EncodeDecodeData]);
 };
+
+
 module.exports = jp_encode_decode;
