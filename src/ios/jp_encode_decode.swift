d6de134914dd63fd6ca56933989810f6efa74174
winlinaung <scm.winlinaung@gmail.com>
2019-12-20 11:14:57 +0630

    first commit

---
+++ src/ios/jp_encode_decode.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+@objc(jp_encode_decode) class jp_encode_decode : CDVPlugin {
+  @objc(EncodeWithSJIS:) // Declare your function name.
+  func EncodeWithSJIS(command: CDVInvokedUrlCommand) { // write the function code.
+            let encoder = JSONEncoder()
+            print("command.arguments![0] = ", command.arguments![0])
+        do {
+            let m = "販売名不明のインスリン製剤ブランク(空文字)".data(using: .shiftJIS)
+            let SJISEncoded_json = try! JSONEncoder().encode(m)
+            let SJISEncoded_jsonString = String(data: SJISEncoded_json, encoding: .shiftJIS)!
+            print("SJISEncoded_jsonString = ", SJISEncoded_jsonString)
+            let base64SJISEncoded = SJISEncoded_jsonString.replacingOccurrences(of: "\"", with: "", options: NSString.CompareOptions.literal, range: nil)
+            var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+            // Set the plugin result to succeed.
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: base64SJISEncoded);
+            // Send the function result back to Cordova.
+            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+        }
+    
+
+  }
+}
---
+++ www/jp_encode_decode.js
@@ -0,0 +1,12 @@
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = "jp_encode_decode"; // This is just for code completion uses.
+
+var jp_encode_decode = function() {}; // This just makes it easier for
+ // us to export all of the functions at once.
+// All of your plugin functions go below this. 
+// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
+jp_encode_decode.yourFunctionName = function(onSuccess, onError) {
+   exec(onSuccess, onError, PLUGIN_NAME, "yourFunctionName", []);
+};
+module.exports = jp_encode_decode;
