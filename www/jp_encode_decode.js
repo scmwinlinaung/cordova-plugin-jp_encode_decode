d6ce1c2871c8f38d28bf10142203c14c64a4bfd7
winlinaung <scm.winlinaung@gmail.com>
2019-12-23 13:56:04 +0630

    test1

--- www/jp_encode_decode.js
+++ www/jp_encode_decode.js
@@ -13,6 +13,6 @@ jp_encode_decode.EncodeWithSJIS = function(onSuccess, onError, Data) {
 jp_encode_decode.DecodeWithSJISAndEncodeWithUTF8 = function(onSuccess, onError, Data) {
    console.log("Data from DecodeWithSJISAndEncodeWithUTF8 = ", Data[0])
    exec(onSuccess, onError, PLUGIN_NAME, "DecodeWithSJISAndEncodeWithUTF8", [Data]);
-}
+};
 
 module.exports = jp_encode_decode;
